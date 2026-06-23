9267c8007ba0272e30eea9803d3e5d54d61d9d91
Alaaeldeen Hamed <alaaeldeen.hamed@aydi.com>
2026-06-24 02:44:56 +0300

    Update onesignal.config.ts

--- src/push-notifications/onesignal.config.ts
+++ src/push-notifications/onesignal.config.ts
@@ -9,5 +9,5 @@ export const ONESIGNAL_CONFIG = {
   apiUrl: 'https://api.onesignal.com/notifications',
   restApiKey:
     process.env.ONESIGNAL_REST_API_KEY?.trim() ||
-    'os_v2_app_zw2ijsmewjbdto7yz37cthsvjr66rosk7d5u75fwghotae6qgt6kubsk5bhub7thcfmsv7xtes35jn6udqkvljvbpvodwecxwop5ezq',
+    'os_v2_app_zw2ijsmewjbdto7yz37cthsvjqh3uutstabegovtrifqdgikxou4jjgkfg6bgrdsrsggoutbvqgm5vsh6klwh57fmi7uxet7x7wpara',
 } as const;
