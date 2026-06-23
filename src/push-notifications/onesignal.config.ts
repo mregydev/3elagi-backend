0b4c5a412a7d45e3f792faef58f29e9af5f104ce
Alaaeldeen Hamed <alaaeldeen.hamed@aydi.com>
2026-06-23 22:07:35 +0300

    fix: add rest api key

--- src/push-notifications/onesignal.config.ts
+++ src/push-notifications/onesignal.config.ts
@@ -6,5 +6,5 @@ export const ONESIGNAL_CONFIG = {
   appId: 'cdb484c9-84b2-4239-bbf8-cefe299e554c',
   apiUrl: 'https://api.onesignal.com/notifications',
   /** OneSignal REST API Key (required for server-side push). */
-  restApiKey: '',
+  restApiKey: 'os_v2_app_zw2ijsmewjbdto7yz37cthsvjrwoprqehkfeybn4jm4omgljdeipxxxzw2ahfmu6fwdrohokha3cjpkubufw32i4g374lddk6epb3ny',
 } as const;
